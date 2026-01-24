acbaa3e485b461aedee44ed21170c3735295884f
shabani005 <hasan.shabani2005@gmail.com>
2026-01-24 13:23:30 +0300

    adding chaos_rebuild later

--- chaos.c
+++ chaos.c
@@ -51,6 +51,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <string.h>
 
 typedef struct {
   char* items;
@@ -74,7 +75,6 @@ typedef struct {
 
 #define CHAOS_TMP_BUF_SIZE 8192
 #define CHAOS_TMP_BUF_COUNT 20
-
 
 
 /*
@@ -143,6 +143,11 @@ CHAOSDEF bool chaos_does_file_exist(char *filename){
   return false;
 }
 
+// CHAOSDEF bool chaos_did_file_change(char *filename){
+//   char* old_file = chaos_temp_sprintf("%s.old", filename);
+//   if (chaos_does_file_exist(old_file)) return true;
+//   return false;
+// }
 
 /*
   ======== STRING RELATED UTILITIES =========
@@ -190,9 +195,72 @@ CHAOSDEF Chaos_String_View chaos_split_by_delim(Chaos_String_View *sv, char deli
   return result;
 }
 
+CHAOSDEF Chaos_String_View chaos_trim_left(Chaos_String_View *sv){
+  Chaos_String_View result = {0};
+
+  result.data = sv->data;
+  result.count = sv->count;
+
+  size_t i=0;
+
+  while (i < result.count && result.data[i] == ' '){
+    i++;
+  }
+
+  result.count -=i;
+  result.data +=i;
+
+  return result;
+}
+
+CHAOSDEF Chaos_String_View chaos_trim_right(Chaos_String_View *sv) {
+  Chaos_String_View result = {0};
+
+  result.data  = sv->data;
+  result.count = sv->count;
+
+  size_t i = result.count;
+
+  while (i > 0 && result.data[i - 1] == ' ') {
+    --i;
+  }
+
+  result.count = i;
+
+  return result;
+}
+
+CHAOSDEF Chaos_String_View chaos_trim(Chaos_String_View *sv){
+  Chaos_String_View tmp = chaos_trim_left(sv);
+  return chaos_trim_right(&tmp);
+}
+
+CHAOSDEF void chaos_sb_append_null(Chaos_String_Builder *sb){
+  chaos_da_append(sb, '\0');  
+}
+
+CHAOSDEF void chaos_sb_append_cstr(Chaos_String_Builder *sb, char* s){
+  while (*s){
+    chaos_da_append(sb, *s++);
+  }
+}
+
+CHAOSDEF void chaos_sb_appendf(Chaos_String_Builder *sb, const char* fmt, ...){
+  CHAOS_TODO("NOT IMPLEMENTED");
+}
 /*
   ================= Build System Utils ===============
 */
+CHAOSDEF void chaos_cmd_append(Chaos_cmd_arr *arr, char* value){
+  size_t len = strlen(value);
+
+  Chaos_String_View sv = {0};
+  sv.data = value;
+  sv.count = len;
+  
+  chaos_da_append(arr, sv);
+}
+
 
 CHAOSDEF bool chaos_cmd_run(Chaos_cmd_arr *arr) {
   #if !defined(__GNUC__) || defined(__clang__)
@@ -243,5 +311,85 @@ CHAOSDEF bool chaos_cmd_run(Chaos_cmd_arr *arr) {
   return ret == 0; 
 }
 
+// void nb_rebuild(int argc, char **argv){
+//   char *filename = "builder.c";
+//   char* cloned_file = chaos_temp_sprintf("%s.old", filename);
+
+//   if (chaos_does_file_exist(cloned_file)){
+//     // printf("%s does exist\n", cloned_file);
+//     if (chaos_did_file_change(filename)){
+//       printf("[Rebuilding]\n");
+//       nb_copy_file(filename, cloned_file);
+
+//       Chaos_cmd_arr cmd = {0};
+//       char *fname = chaos_temp_sprintf("%s.old", filename);
+
+//       chaos_cmd_append(&cmd, "gcc");
+//       chaos_cmd_append(&cmd, "-o");
+//       chaos_cmd_append(&cmd, fname);
+//       chaos_cmd_append(&cmd, filename);
+      
+//       chaos_cmd_run(&cmd);
+
+//       printf("[INFO] rebuilt %s\n\n", filename);
+
+//       for (size_t i=0; i<argc; ++i){
+//         chaos_cmd_append(&cmd, argv[i]);
+//       }
+//       chaos_cmd_run((&cmd);
+//             exit(1);
+
+//   } else {
+//     // printf("file did not change\n");
+//     }
+//   }else{
+//     nb_copy_file(filename, cloned_file);
+//   }
+// }
+
+/*
+  =================== MISC Utilities ===================
+*/
+
+CHAOSDEF bool chaos_is_float(char* v){
+  size_t len = strlen(v);
+  size_t dots = 0;
+  size_t sign = 0;
+  
+  if (len==0) return false;
+
+  if (v[0] == '.' || v[len-1] == '.') return false;
+
+  if (v[0] == '-' || v[0] == '+') sign++;
+  
+  for (size_t i=0; i<len; ++i){
+    if (dots > 1 || sign > 1) return false;
+    if (v[i] == '.') {
+      dots++;
+      continue;
+    } else if (v[i] == '-' || v[i] == '+') {
+      sign++;
+      continue;
+    }
+    if ((unsigned char)v[i] < '0' || (unsigned char)v[i] > '9') return false;
+  }
+  return true;
+}
+
+CHAOSDEF bool chaos_is_int(char* v){
+  size_t len = strlen(v);
+  size_t sign = 0;
+
+  if (len==0) return false;
+
+  if (v[0] == '+' || v[0] == '-') sign++;
+  
+  for (size_t i=0; i<len; ++i){
+    if (sign > 1) return false;
+    if ((unsigned char)v[i] < '0' || (unsigned char)v[i] > '9') return false;
+    else if (v[0] == '+' || v[0] == '-') sign++;
+  }
+  return true;
+}
 #endif // CHAOS_H_
  
